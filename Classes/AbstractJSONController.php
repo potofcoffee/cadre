819feba372bbabbb333e61cdb53e49dd59ea422d
Christoph Fischer <chris@toph.de>
2016-04-04 12:26:05 +0200

    Added AbstractJSONController

---
+++ Classes/AbstractJSONController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Peregrinus\Cadre;
+
+/* 
+ * Copyright (C) 2016 chris
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class AbstractJSONController extends \Peregrinus\Cadre\AbstractController {
+    
+    protected function initializeView() {
+        // No view will be available! Be sure not to use $this->view->...!
+    }
+
+    protected function initializeController() {
+        $this->view->setContentType('application/json');        
+        $this->setEncodingFunction('json_encode');
+    }
+    
+    
+}
